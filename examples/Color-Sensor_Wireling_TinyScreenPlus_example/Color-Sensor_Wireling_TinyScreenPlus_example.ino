cdac66e2244bba06ad6cc78ceae3b9829b4c02e7
Rena <info@tinycircuits.com>
2019-12-18 16:37:59 -0500

    update program

--- examples/Color-Sensor_Wireling_TinyScreenPlus_example/Color-Sensor_Wireling_TinyScreenPlus_example.ino
+++ examples/Color-Sensor_Wireling_TinyScreenPlus_example/Color-Sensor_Wireling_TinyScreenPlus_example.ino
@@ -1,7 +1,7 @@
 /*************************************************************************
  * Color Sensor TCS34725 Wireling Tutorial:
  * This program will print the TCS34725 RGB, lux, and clearness values to 
- * the Serial Monitor
+ * the Serial Monitor and a TinyScreen+
  * 
  * Note: I²C Address for Wireling board is different than the Adafruit 
  * component, this address change is reflected in the library that
@@ -15,6 +15,7 @@
  ************************************************************************/
 
 #include <Wire.h>         // For I2C communication with sensor
+#include <TinyScreen.h>   // For interfacing with the TinyScreen+
 #include "Adafruit_TCS34725.h"  // The library used for the Color Sensor
 #include <Wireling.h>     // For interfacing with Wirelings
 
@@ -31,6 +32,10 @@
 /* Initialise with specific int time and gain values */
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 
+// TinyScreen+ variables
+TinyScreen display = TinyScreen(TinyScreenPlus);
+int background = TS_8b_Black;
+
 // Variables to hold the values the sensor reads
 uint16_t r, g, b, c, colorTemp, lux;
 
@@ -38,6 +43,13 @@ void setup(void) {
   SerialMonitorInterface.begin(9600);
   Wire.begin();
   Wireling.begin();
+
+  // Setup and style for TinyScreen+
+  display.begin();
+  display.setFlip(true);
+  display.setBrightness(15);
+  display.setFont(thinPixel7_10ptFontInfo);
+  display.fontColor(TS_8b_White, background);
 
   //The port is the number on the Adapter board where the sensor is attached
   Wireling.selectPort(0);
@@ -66,6 +78,8 @@ void loop(void) {
   SerialMonitorInterface.print("Clr: "); SerialMonitorInterface.print(c, DEC);
   SerialMonitorInterface.println(" ");
 
+  printScreen();
+
   delay(500);
 }
 
@@ -77,4 +91,41 @@ void LEDon() {
 // Turn Wireling LEDs off
 void LEDoff() {
   tcs.setInterrupt(false);
+}
+
+// Prints the color sensor values to the TinyScreen
+void printScreen(void){
+  // This will make the screen look a little unsteady but is needed in order
+  // to clear old values 
+  display.clearScreen();
+  
+  display.fontColor(TS_8b_DarkRed, background);
+  display.setCursor(0, 0);
+  display.print("ColorTemp = ");
+  display.print(colorTemp);
+
+  display.fontColor(TS_8b_Yellow, background);
+  display.setCursor(0, 11);
+  display.print("Lux = ");
+  display.print(lux);
+
+  display.fontColor(TS_8b_Red, background);
+  display.setCursor(0, 22);
+  display.print("R = ");
+  display.print(r);
+
+  display.fontColor(TS_8b_Green, background);
+  display.setCursor(0, 33);
+  display.print("G = ");
+  display.print(g);
+
+  display.fontColor(TS_8b_Blue, background);
+  display.setCursor(0, 44);
+  display.print("B = ");
+  display.print(b);
+
+  display.fontColor(TS_8b_White, background);
+  display.setCursor(0, 55);
+  display.print("Clr = ");
+  display.print(c);
 }
